b66319a2c91b397348690d9c8d1421e1a09ce392
MatthewMawby <themesta@gmail.com>
2017-02-15 14:59:12 -0500

    Webview is hidden when authentication attempt is being made

--- src/CASLogin.js
+++ src/CASLogin.js
@@ -16,7 +16,8 @@ import { Button, Toolbar } from 'react-native-material-design';
 export default class Signin extends Component{
 
   state: {
-    attemptingAuth: bool
+    attemptingAuth: bool,
+    webFlex: 1
   };
 
   constructor(){
@@ -28,17 +29,20 @@ export default class Signin extends Component{
 
   whenNavigationStateChanges(navState: any){
     var navigator = this.props.navigator;
+    if (navState.url != venue.getDomain() + "/auth/cas?mobile=true"){
+        this.setState({attemptingAuth:true});
+    }
     fetch(navState.url, {
       method: 'GET'
     }).then((response) => {
       return response.json();
     }).then((json) => {
       if (json['token']){
-        if (!this.state.attemptingAuth){
-          venue.authenticateWithToken(json['token']).then(() => {
+        if (this.state.attemptingAuth){
+            venue.authenticateWithToken(json['token']).then(() => {
             navigator.resetTo({title: "dashboard"});
           });
-          this.setState({attemptingAuth:true});
+
         }
       }
     }).catch((err,arg2) => {
@@ -48,21 +52,23 @@ export default class Signin extends Component{
   }
 
   render(){
-    return (
-      <View style={styles.container}>
-        <Toolbar
-          title={"venue - Login with CAS"}/>
-        <WebView
-          style={styles.webview}
-          source={{uri: venue.getDomain() + "/auth/cas?mobile=true"}}
-          onNavigationStateChange={this.whenNavigationStateChanges.bind(this)}
-          >
-          CAS Login Page
-        </WebView>
-      </View>
-    );
-  }
+
+        return (
+          <View style={styles.container}>
+            <Toolbar
+              title={"venue - Login with CAS"}/>
+            <WebView
+              style={(this.state.attemptingAuth) ? styles.hidden : styles.webview}
+              source={{uri: venue.getDomain() + "/auth/cas?mobile=true"}}
+              onNavigationStateChange={this.whenNavigationStateChanges.bind(this)}
+              >
+              CAS Login Page
+            </WebView>
+          </View>
+        );
+    }
 }
+
 
 
 
@@ -72,6 +78,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
   webview: {
-    marginTop:55
+    marginTop:55,
+    flex: 1
+  },
+  hidden:{
+      flex: 0
+  },
+  text: {
+      flex: 1,
+      fontSize: 30
   }
 });
